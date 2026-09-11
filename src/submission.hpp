c3962aa89ee80df4a6f170aa824b7dbc7a54d529
Leon <leonzh1018@gmail.com>
2026-09-10 21:40:48 -0400

    removed checking for boundaries each entry and minor optimizations

--- src/submission.hpp
+++ src/submission.hpp
@@ -15,45 +15,36 @@ private:
   std::size_t cols_;
   std::vector<double> grid_;
 
-  bool index_is_valid (std::size_t i, std::size_t j) const;
   std::size_t get_rows() const;
   std::size_t get_cols() const;
-  double& get_index(std::size_t i, std::size_t j);
-  double const_get_index(std::size_t i, std::size_t j) const;
-  void change_rows(const std::size_t new_rows);
-  void change_cols(const std::size_t new_cols);
+  double& get_index(const std::size_t& i, const std::size_t& j);
+  const double& get_index(const std::size_t& i, const std::size_t& j) const;
   
   
 
   
 public:
   Grid(std::size_t rows, std::size_t cols);
+  
+  double& operator()(const std::size_t& i, const std::size_t& j);
+  double  operator()(const std::size_t& i, const std::size_t& j) const;
+  std::vector<std::size_t> get_dims() const;
+  
+  
+};
 
-  double& operator()(std::size_t i, std::size_t j);
-  double  operator()(std::size_t i, std::size_t j) const;
-  const std::size_t* get_dims() const;
-};  
+const bool validate(const Grid& grid1, const Grid& grid2);
+  
 
-inline const std::size_t* Grid::get_dims() const
+inline std::vector<std::size_t> Grid::get_dims() const
 {
-	std::size_t rows = this->get_rows();
-	std::size_t cols = this->get_cols();
-	const std::size_t* dims = new std::size_t [2] {rows, cols};
+	const std::vector<std::size_t> dims {get_rows(), get_cols()};
 	return dims;
 }
 
 inline Grid::Grid(std::size_t rows, std::size_t cols)
 	: rows_(rows) , cols_(cols), grid_(rows * cols)
 {}
-
-inline bool Grid::index_is_valid(std::size_t i, std::size_t j) const{
-	if (i >= this->get_rows() || j >= this->get_cols())
-	{
-		return false;
-	}
-	return true;
-
-}
 
 inline std::size_t Grid::get_rows() const
 {
@@ -67,31 +58,32 @@ inline std::size_t Grid::get_cols() const
 
 
 
-inline double& Grid::get_index(std::size_t i, std::size_t j)
+inline double& Grid::get_index(const std::size_t& i, const std::size_t& j)
 {
 	return grid_[i*(this->get_cols()) + j];
 }
 
-inline double Grid::const_get_index(std::size_t i, std::size_t j) const
+inline const double& Grid::get_index(const std::size_t& i, const std::size_t& j) const
 {
 	return grid_[(i*this->get_cols()) + j];
 }
 
-inline double& Grid::operator()(std::size_t i, std::size_t j)
+inline double& Grid::operator()(const std::size_t& i, const std::size_t& j)
 {
 
-	double& result = this->get_index(i,j);
-	return result;
+	return get_index(i,j);
 }
 
-inline double Grid::operator()(std::size_t i, std::size_t j) const
+inline double Grid::operator()(const std::size_t& i, const std::size_t& j) const
 {
-	const double result = this->const_get_index(i,j);
-	return result;
+	return get_index(i,j);
 }
 
 
-
+inline const bool validate(const Grid& grid1, const Grid& grid2)
+{
+	return grid1.get_dims() == grid2.get_dims();
+}
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
 void apply_stencil(const Grid& old_grid, Grid& new_grid);
@@ -99,24 +91,25 @@ void apply_stencil(const Grid& old_grid, Grid& new_grid);
 
 void apply_stencil(const Grid& old_grid, Grid& new_grid)
 {	
-	const std::size_t* dims = old_grid.get_dims();
-	std::size_t rows = *(dims);
-	std::size_t cols = *(dims + 1);
-	for (std::size_t i = 0; i < rows; ++i)
+	const std::vector<std::size_t>& dims = old_grid.get_dims();
+	const std::size_t& rows = dims[0];
+	const std::size_t& cols = dims[1];
+	for (std::size_t i {1}; i < rows; ++i)
 	{
-		for (std::size_t j = 0; j < cols; ++j)
+		for (std::size_t j {1}; j < cols; ++j)
 		{
-			if (i == 0 || j == 0 || i == rows-1 || j == cols-1)
-			{
-				new_grid(i,j) = old_grid(i,j);
-			}
-			else
-			{
-				new_grid(i,j) = 0.5*old_grid(i,j) + 0.125 * ( old_grid(i-1,j) + old_grid(i+1, j) + old_grid(i,j+1) + old_grid(i,j-1));
-			}
+			new_grid(i,j) = 0.5*old_grid(i,j) + 0.125 * ( old_grid(i-1,j) + old_grid(i+1, j) + old_grid(i,j+1) + old_grid(i,j-1));
 		}
 	}
-	delete[] dims;
-	dims=nullptr;
+	for (std::size_t i {}; i < rows; ++i)
+	{
+		new_grid(i,0) = old_grid(i,0);
+		new_grid(i,cols-1) = old_grid(i,cols-1);
+	}
+	for (std::size_t j {}; j < cols; ++j)
+	{
+		new_grid(0,j) = old_grid(0,j);
+		new_grid(rows-1,j) = old_grid(rows-1, j);
+	}
 	return;
 }
